cb0c94f0ea83f64286a230f07210ecade94adbba
Márcia <rcsilva.marcia@gmail.com>
2017-06-07 22:44:50 -0300

    Inserção

--- functions.c
+++ functions.c
@@ -1,1 +1,47 @@
+void inicializar(unrolled_list_t** l, construtor_t construtor, desconstrutor_t desconstrutor)
+{
+
+    (*l) = malloc(sizeof (unrolled_list_t));
+    (*l)->numElementos = NULL;
+    (*l)->elementos = 0;
+    (*l)->construtor = construtor;
+    (*l)->desconstrutor = desconstrutor;
+}
+noLista* lista_novo(void* data, construtor_t c)
+{
+
+    noLista* novo_no;
+    novo_no = malloc(sizeof(noLista));
+    novo_no->dado = c(dado);
+    novo_no->proximo = NULL;
+    return novo_no;
+}
+void unrolled_list_insert(unrolled_list_t* l,size_t i,void* data)
+{
+    //Se a lista estiver vazia cria um novo no e adiciona na posição 0 do vetor
+    if(l->numElementos == NULL)
+    {
+        noLista* novo_no = lista_novo(data, l->construtor);
+        l->elementos[0] = data;
+        l->elementos++;
+    }
+    if(l->numElementos+1 < elementos)
+    {
+        noLista* novo_no = lista_novo(data, l->construtor);
+        l->elementos[l->numElementos] = data;
+        l->numElementos++;
+    }
+    else
+    {
+        noLista* novo_no = lista_novo(data, l->construtor);
+        int j = 0;
+        for(i = 0; l->numElementos / 2 + 1; i < l->numElementos; i++)
+        {
+            l->numElementos[j++] = l->elementos[i];
+            l->elementos[j++] = data;
+            l->numElementos = l->numElementos/2 + 1;
+            novo_no->proximo = l;
+        }
+    }
+}
 
--- lib.h
+++ lib.h
@@ -1,19 +1,28 @@
 #define LIB_H
 
 #include <stdlib.h>
+#define TOTAL 5
 
 typedef void* (*construtor_t)(void*);
 typedef void (*desconstrutor_t)(void*);
 
-
 typedef struct noLista
 {
-    int elementos;                //total de elementos da lista
-    int array[total];            // um array de elementos total, com espaço para um total de elementos
-    struct noLista* proximo;    // referência ao próximo nó na lista
+    void* data;                         //Dado á ser inserido na lista
+    struct noLista* proximo;           // referência ao próximo nó na lista
 } noLista;
 
+typedef struct Lista
+{
+    int numElementos;                    //total de elementos da lista
+    int elementos[TOTAL];                // um array de elementos total, com espaço para um total de elementos
+    construtor_t construtor;        //Construtor constroi uma lista de um tipo generico
+    desconstrutor_t desconstrutor; // Libera memoria
+} unrolled_list_t;
+
+void inicializar(unrolled_list_t** l, construtor_t construtor, desconstrutor_t desconstrutor); //Inicializa a minha lista com null e 0
+noLista* lista_novo(void* data, construtor_t c); //cria um novo nó na lista
 void unrolled_list_insert(unrolled_list_t* l,size_t i,void* data); //insere o dado data na i-ésima posição da lista
 void unrolled_list_remove(unrolled_list_t* l,size_t i,void* data)/ //remove o dado contido na i-ésima posição da lista
-void* unrolled_list_access(unrolled_list_t* l, size_t i); //retorna o dadocontido nai-ésima posição da lista
+void* unrolled_list_access(unrolled_list_t* l, size_t i); //retorna o dado contido na i-ésima posição da lista
 #endif
